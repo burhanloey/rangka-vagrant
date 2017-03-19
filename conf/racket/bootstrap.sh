317ac13110d8ce9b9f41d8b6be833d07b68769be
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-03-19 08:07:00 +0800

    Remove sudo since vagrant provision already run as sudo

--- conf/racket/bootstrap.sh
+++ conf/racket/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:plt/racket
+add-apt-repository ppa:plt/racket
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y racket
+apt-get install -y racket
--- conf/sbcl/bootstrap.sh
+++ conf/sbcl/bootstrap.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo apt-get update
+apt-get update
 
-sudo apt-get install -y sbcl
+apt-get install -y sbcl
 
-sudo apt-get install -y rlwrap
+apt-get install -y rlwrap
 
 curl https://beta.quicklisp.org/quicklisp.lisp > /tmp/quicklisp.lisp
