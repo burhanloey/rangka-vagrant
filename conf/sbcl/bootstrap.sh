d239ac09fef001b46712c43cc794e5db399174d2
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-03-19 15:28:21 +0800

    Add sample script to start sbcl app

--- conf/sbcl/bootstrap.sh
+++ conf/sbcl/bootstrap.sh
@@ -7,3 +7,7 @@ apt-get install -y sbcl
 apt-get install -y rlwrap
 
 curl https://beta.quicklisp.org/quicklisp.lisp > /tmp/quicklisp.lisp
+
+cp /vagrant/conf/sbcl/myapp-start /usr/local/bin/
+
+chmod a+x /usr/local/bin/myapp-start
