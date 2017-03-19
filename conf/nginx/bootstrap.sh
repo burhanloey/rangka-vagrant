e9ddf114a9c0816fd673a5100e25ecb8cf65e005
Burhanuddin Baharuddin <burhan_loey@yahoo.com.my>
2017-03-19 09:28:02 +0800

    Provision nginx configuration for loadbalancer

--- conf/nginx/bootstrap.sh
+++ conf/nginx/bootstrap.sh
@@ -3,3 +3,19 @@
 apt-get update
 
 apt-get install -y nginx
+
+cp /vagrant/conf/nginx/loadbalancer /etc/nginx/sites-available/
+
+echo "Copied loadbalancer config file into /etc/nginx/sites-available/"
+
+rm /etc/nginx/sites-enabled/default
+
+echo "Removed default config symlink"
+
+ln -s /etc/nginx/sites-available/loadbalancer /etc/nginx/sites-enabled/
+
+echo "Created symlink for loadbalancer"
+
+service nginx reload
+
+echo "Reloaded nginx with new configuration"
